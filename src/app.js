6ddc1fe1570fd5ba351138ddc57a7aea23a45781
sravankarapakula <sravankarapakula2006@gmail.com>
2026-07-23 16:02:23 +0530

    app.js configurations

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+import cookieParse from "cookie-parser"
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:'16kb'}))
+app.use(express.urlencoded({extende:true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParse())
+
+
+
+export { app } 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path:"./.env"
 })
 
-connectDB();
+const port = process.env.PORT || 8000
 
+connectDB()
+.then(()=>{
+    app.on("error",(e)=>{
+        console.log("ERROR: ",e);
+        throw error;
+    })
+
+    app.listen(port, ()=>{
+        console.log(`Server running on port: ${port}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED !!!",err);
+    
+})
 
 
 
